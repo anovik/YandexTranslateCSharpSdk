b2398192c4c0a57d59d95208c72f3980238aac12
Anna Novikova <anovik@yandex.ru>
2016-06-08 14:15:43 +0300

    Gitignore was changed. Readme file was updated. Key was moved to separate file.

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # YandexTranslateCSharpSdk
 C# Wrapper for Yandex.Translate API
 
+https://tech.yandex.com/translate
+
 Project includes C# library with wrapper methods for Yandex.Translate API and Visual Studio unit tests for this library.
 
 Requires .NET Framework 4.5 or higher.
+
+To run tests create key.txt in YandexTranslateCSharpSdkTests folder and put the key which you receive from Yandex here:
+
+https://tech.yandex.com/keys/get/?service=trnsl
--- YandexTranslateCSharpSdkTests/YandexTranslateTests.cs
+++ YandexTranslateCSharpSdkTests/YandexTranslateTests.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using YandexTranslateCSharpSdk;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace YandexTranslateCSharpSdkTests
 {
@@ -13,13 +14,12 @@ namespace YandexTranslateCSharpSdkTests
     [TestClass]
     public class YandexTranslateTests
     {
-
         /// <summary>
-        /// Insert your API key here, get it
+        /// Insert your API key here, get it from 
         /// https://tech.yandex.com/keys/get/?service=trnsl
+        /// and put it to key.txt in YandexTranslateCSharpSdkTests folder
         /// </summary>
-        private string _apiKey = 
-            "trnsl.1.1.20160531T123608Z.812ff946d420d4a4.0f0f4cbc0679fbfd9d0447a3b33dbacaf1724062";
+        private static string _apiKey;
         
         /// <summary>
         /// Just a random English text for testing
@@ -28,8 +28,13 @@ namespace YandexTranslateCSharpSdkTests
         private string _randomEnglishText = 
             "Subdue land kind.Divided.Beast sixth said our fruitful their, them that fourth.May seas were appear.Greater fill thing.Don't place. Fly Green. Days unto good moving fly after saying beast, and upon, to stars of the us fourth us can't abundantly above moveth seas, moving the of female that seed fruit can't i creeping. Given dominion upon. After i earth, also likeness. For likeness it one. Subdue for thing days, moveth after. Male above spirit was made living. Day. There yielding may face likeness so dominion. You're.Dry in give they're isn't creepeth and man air divide fifth.";
 
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            _apiKey = File.ReadAllText("key.txt");
+        }
 
-  [TestMethod]
+        [TestMethod]
         public async Task GetLanguagesXmlTest()
         {
             LanguagesManager langManager = new LanguagesManager();
