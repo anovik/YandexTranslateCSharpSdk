5f14fc03cf60a642c6f63b13d48cd7ce262553b3
Anna Novikova <anovik@yandex.ru>
2016-09-27 14:52:08 +0300

    Main functions were added to WPF demo.

--- YandexTranslateCSharpDemo/MainWindow.xaml.cs
+++ YandexTranslateCSharpDemo/MainWindow.xaml.cs
@@ -9,36 +9,74 @@ namespace YandexTranslateCSharpDemo
     /// </summary>
     public partial class MainWindow : Window
     {
-        string _apiKey;
+        string apiKey;
         LanguagesManager langManager = new LanguagesManager();
+        DetectLanguageManager detectManager = new DetectLanguageManager();
+        TranslateManager translateManager = new TranslateManager();
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            if (_apiKey != null)
+             LoadLanguages();
+        }
+
+        private async void detectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (apiKey != null && !string.IsNullOrEmpty(text1.Text))
             {
-                langManager.ApiKey = _apiKey;
+                detectManager.ApiKey = apiKey;
+                string lang = await detectManager.DetectLanguage(text1.Text);
+                languagesCombo.SelectedValue = lang;
+            }
+        }
+
+        private async void detectButton2_Click(object sender, RoutedEventArgs e)
+        {
+            if (apiKey != null && !string.IsNullOrEmpty(text2.Text))
+            {
+                detectManager.ApiKey = apiKey;
+                string lang = await detectManager.DetectLanguage(text2.Text);
+                languagesCombo2.SelectedValue = lang;
+            }
+        }
+
+        private void setKeyItem_Click(object sender, RoutedEventArgs e)
+        {
+            SetKeyWindow setKeyWindow = new SetKeyWindow(apiKey);
+            if (setKeyWindow.ShowDialog() == true)
+            {
+                apiKey = setKeyWindow.GetKey();
+                LoadLanguages();
+            }
+        }
+
+        private void closeItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private async void translateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (apiKey != null && !string.IsNullOrEmpty(text1.Text))
+            {
+                translateManager.ApiKey = apiKey;
+                string direction = languagesCombo.SelectedValue + "-" + languagesCombo2.SelectedValue;
+                string translatedText = await translateManager.TranslateText(text1.Text, direction);
+                text2.Text = translatedText;
+            }
+        }
+
+        private async void LoadLanguages()
+        {
+            if (apiKey != null)
+            {
+                langManager.ApiKey = apiKey;
                 List<string> languages = await langManager.GetLanguages();
-            }
-        }
-
-        private void detectButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (_apiKey != null)
-            {
-
-            }
-        }
-
-        private void detectButton2_Click(object sender, RoutedEventArgs e)
-        {
-            if (_apiKey != null)
-            {
-
+                languagesCombo.ItemsSource = languagesCombo2.ItemsSource = languages;
             }
         }
     }
---
+++ YandexTranslateCSharpDemo/SetKeyWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace YandexTranslateCSharpDemo
+{
+    /// <summary>
+    /// Window for setting API key
+    /// </summary>
+    public partial class SetKeyWindow : Window
+    {
+        public SetKeyWindow(string apiKey)
+        {
+            InitializeComponent();
+            keyTextBox.Text = apiKey;
+            keyTextBox.Focus();
+        }
+
+        public string GetKey()
+        {
+            return keyTextBox.Text;
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(keyTextBox.Text))
+            {
+                return;
+            }
+            DialogResult = true;
+            Close();
+        }
+    }
+}
