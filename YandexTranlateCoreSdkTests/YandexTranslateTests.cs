d0e71448d74c3a8f3c63c4343e10ebbd101adcc6
Anna Novikova <anovik@yandex.ru>
2017-06-26 11:46:56 +0300

    Core tests were finished.

--- YandexTranlateCoreSdkTests/YandexTranslateTests.cs
+++ YandexTranlateCoreSdkTests/YandexTranslateTests.cs
@@ -1,12 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using Xunit;
+using YandexTranslateCSharpSdk;
 
 namespace YandexTranlateCoreSdkTests
 {
     public class YandexTranslateTests
     {
-        [Fact]
-        public void Test1()
+        /// <summary>
+        /// Insert your API key here, get it from 
+        /// https://tech.yandex.com/keys/get/?service=trnsl
+        /// and put it to key.txt in YandexTranlateCoreSdkTests folder
+        /// </summary>
+        private static string _apiKey;
+
+        /// <summary>
+        /// Just a random English text for testing
+        /// http://www.dummytextgenerator.com
+        /// </summary>
+        private string _randomEnglishText =
+            "Subdue land kind.Divided.Beast sixth said our fruitful their, them that fourth.May seas were appear.Greater fill thing.Don't place. Fly Green. Days unto good moving fly after saying beast, and upon, to stars of the us fourth us can't abundantly above moveth seas, moving the of female that seed fruit can't i creeping. Given dominion upon. After i earth, also likeness. For likeness it one. Subdue for thing days, moveth after. Male above spirit was made living. Day. There yielding may face likeness so dominion. You're.Dry in give they're isn't creepeth and man air divide fifth.";
+
+
+        public YandexTranslateTests()
         {
+            _apiKey = File.ReadAllText("key.txt");
+        }   
+
+        [Fact]
+        public async Task GetLanguagesXmlTest()
+        {
+            YandexTranslateSdk wrapper = new YandexTranslateSdk();
+            wrapper.ApiKey = _apiKey;
+            wrapper.IsJson = false;
+            List<string> languages = await wrapper.GetLanguages();
+            Assert.NotEqual(languages, null);
+            Assert.True(languages.Contains("en"));
+            Assert.True(languages.Contains("ru"));
+        }
+
+        [Fact]
+        public async Task GetLanguagesJsonTest()
+        {
+            YandexTranslateSdk wrapper = new YandexTranslateSdk();
+            wrapper.ApiKey = _apiKey;
+            wrapper.IsJson = true;
+            List<string> languages = await wrapper.GetLanguages();
+            Assert.NotEqual(languages, null);
+            Assert.True(languages.Contains("en"));
+            Assert.True(languages.Contains("ru"));
+        }
+
+        [Fact]
+        public async Task DetectLanguageXmlTest()
+        {
+            YandexTranslateSdk wrapper = new YandexTranslateSdk();
+            wrapper.ApiKey = _apiKey;
+            wrapper.IsJson = false;
+            string result = await wrapper.DetectLanguage("Привет");
+            Assert.Equal(result, "ru");
+        }
+
+        [Fact]
+        public async Task DetectLanguageJsonTest()
+        {
+            YandexTranslateSdk wrapper = new YandexTranslateSdk();
+            wrapper.ApiKey = _apiKey;
+            wrapper.IsJson = true;
+            string result = await wrapper.DetectLanguage(_randomEnglishText);
+            Assert.Equal(result, "en");
+        }
+
+        [Fact]
+        public async Task TranslateXmlTest()
+        {
+            YandexTranslateSdk wrapper = new YandexTranslateSdk();
+            wrapper.ApiKey = _apiKey;
+            wrapper.IsJson = false;
+            string translatedText = await wrapper.TranslateText(_randomEnglishText, "en-ru");
+            Console.WriteLine(translatedText);
+            Assert.True(!string.IsNullOrEmpty(translatedText));
+        }
+
+        [Fact]
+        public async Task TranslateJsonTest()
+        {
+            YandexTranslateSdk wrapper = new YandexTranslateSdk();
+            wrapper.ApiKey = _apiKey;
+            wrapper.IsJson = true;
+            string translatedText = await wrapper.TranslateText(_randomEnglishText, "en-ru");
+            Console.WriteLine(translatedText);
+            Assert.True(!string.IsNullOrEmpty(translatedText));
+        }
+
+        [Fact]        
+        public async Task CheckBadKey()
+        {
+            YandexTranslateSdk wrapper = new YandexTranslateSdk();
+            wrapper.ApiKey = "KeyNotExists";            
+            await Assert.ThrowsAsync<YandexTranslateException>(() => wrapper.GetLanguages());
+        }
+
+        [Fact]        
+        public async Task CheckNullKey()
+        {
+            YandexTranslateSdk wrapper = new YandexTranslateSdk();
+            wrapper.ApiKey = null;
+            await Assert.ThrowsAsync<YandexTranslateException>(() => wrapper.GetLanguages());
+        }
+
+        [Fact]
+        
+        public async Task CheckBadDirection()
+        {
+            YandexTranslateSdk wrapper = new YandexTranslateSdk();
+            wrapper.ApiKey = _apiKey;
+            wrapper.IsJson = true;
+            await Assert.ThrowsAsync<YandexTranslateException>(() => wrapper.TranslateText(_randomEnglishText, "BAD"));            
         }
     }
 }
