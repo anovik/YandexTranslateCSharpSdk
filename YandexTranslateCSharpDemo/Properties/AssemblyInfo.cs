430955968e0bd08ed53bc63491125df1fb2b5c7e
Anna Novikova <anna@elangroupsoftware.com>
2019-03-15 12:26:51 +0300

    AssemblyInfo was updated for 1.1.0 version.

--- YandexTranslateCSharpDemo/Properties/AssemblyInfo.cs
+++ YandexTranslateCSharpDemo/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("YandexTranslateCSharpDemo")]
-[assembly: AssemblyCopyright("Copyright ©2016-2017 Anna Novikova")]
+[assembly: AssemblyCopyright("Copyright ©2016-2019 Anna Novikova")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
